3bb279ef94d15d95177bccd6bda737a265cb12cd
Ivan <aw4ken1ngs@gmail.com>
2023-08-17 23:02:34 +0400

    work counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+
+const [counter, setCounter] = React.useState(0);
+
+const counterPlus = () => {
+  setCounter(counter + 1)
+}
+
+const counterMinus = () => {
+  setCounter(counter - 1)
+}
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{counter}</h1>
+        <button onClick={counterMinus} className="minus">- Минус</button>
+        <button onClick={counterPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
